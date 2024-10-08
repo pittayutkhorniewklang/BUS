a401dfa33296356d9e83eb4605c1eb2799dfa3be
Aomsin46 <Nuntaphop.b@ku.th>
2024-10-09 00:47:10 +0700

    totot

--- src/app/pages/reservation/reservation.component.ts
+++ src/app/pages/reservation/reservation.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrl: './reservation.component.css'
 })
 export class ReservationComponent {
-
+confirmSelection() {
+throw new Error('Method not implemented.');
 }
+ 
+ seats = Array(20).fill(null).map(() => ({ selected: false }));
+ toggleSeat(index: number) {
+  this.seats[index].selected = !this.seats[index].selected; // เปลี่ยนสถานะที่นั่ง
+
+  confirmSelection() ;{
+    const selectedSeats = this.seats
+      .map((seat, index) => (seat.selected ? index + 1 : null))
+      .filter(seat => seat !== null);
+    
+    if (selectedSeats.length > 0) {
+      alert(`คุณเลือกที่นั่ง: ${selectedSeats.join(', ')}`);
+    } else {
+      alert('กรุณาเลือกที่นั่งก่อน!');
+    }
+  
+}
+
+  
+
+   function confirmSelection() {
+     throw new Error('Function not implemented.');
+   }
+  }
+};
